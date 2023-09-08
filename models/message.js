3926d58014072fbbdfc846be889f00a9961a93e1
Jordan <jordansampson23@gmail.com>
2023-09-08 09:59:03 -0500

    Models initialized.

---
+++ models/message.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+    title: { type: String, maxLength: 200, required: true },
+    body: { type: String, maxLength: 12000, required: true },
+    timestamp: { type: Date, required: true },
+    author: { type: Schema.Types.ObjectId, ref: 'User', required: true }
+});
---
+++ models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username: { type: String, maxLength: 100, required: true },
+    password: { type: String, maxLength: 100, required: true },
+    firstName: { type: String, maxLength: 100, required: true },
+    lastName: { type: String, maxLength: 100, required: true },
+    date_joined: { type: Date, required: true },
+    membership_status: { type: String, enum: ['user', 'club_member', 'admin'], required: true }
+});
+
+UserSchema.virtual('url').get(function () {
+    return `/users/${this._id}`;
+});
+
+UserSchema.virtual('name').get(function () {
+    return `${this.firstName} ${this.lastName}`;
+});
