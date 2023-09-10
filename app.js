f6bf4c7a016e1e4c69f3047d715cae123bcf9437
Jordan <jordansampson23@gmail.com>
2023-09-10 10:57:18 -0500

    add messagerouter

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ async function main() {
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/usersRouter');
+var messagesRouter = require('./routes/messagesRouter');
 
 var app = express();
 
@@ -47,6 +48,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/messages', messagesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
