16e25c3dead63a88a703f2c2858898b8cd38227b
Jordan <jordansampson23@gmail.com>
2023-09-10 10:57:09 -0500

    exports

--- models/message.js
+++ models/message.js
@@ -7,3 +7,5 @@ const MessageSchema = new Schema({
     timestamp: { type: Date, required: true },
     author: { type: Schema.Types.ObjectId, ref: 'User', required: true }
 });
+
+module.exports = mongoose.model('Message', MessageSchema);
